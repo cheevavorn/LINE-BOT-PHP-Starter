77ba1fbe38246bddb7c5083460fd553ecd2b732f
cheevavorn <mean.mea2@gmail.com>
2018-02-27 18:07:15 +0700

    Update db.php

--- db.php
+++ db.php
@@ -7,9 +7,12 @@
   $db = substr($url["path"], 1);
 
   $conn = new mysqli($server, $username, $password, $db);
-  if($conn){
-    echo "connected";
-  }else{
-    echo "error";
+  if(!$conn){
+    die("error");
    }
+  $query_str = "SELECT * FROM tbl_user WHERE status = 'A'";
+  $result = $conn->query($query_str);
+  while($row = $result->fetch_assoc()){
+    echo $row['id']." ".$row['username']." ".$row['password']."<br/>";
+  }
 ?>
