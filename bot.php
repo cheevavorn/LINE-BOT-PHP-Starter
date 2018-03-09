5f905026c027a2b68339a9b4a04bf692955cee25
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-03-09 22:56:16 +0700

    set to array type

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
+			$messages = array(
 				// emoji text
 				// 'type' => 'message',
 				// 'text' => '\uDBC0\uDC84 LINE emoji'
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 				"type" => "sticker",
 				"packageId" => "1",
 				"stickerId" => "1"
-			];
+			);
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
