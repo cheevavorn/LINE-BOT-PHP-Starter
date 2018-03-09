552b1503f053e77481c21673fdcf503e5c7ee920
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-03-09 22:44:10 +0700

    post back image

--- bot.php
+++ bot.php
@@ -18,15 +18,19 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $event['message']
+				'type' => 'image',
+				'text' => '7598060490135'
 			];
+			// $messages = [
+			// 	'type' => 'text',
+			// 	'text' => $text
+			// ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$event['message']],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
